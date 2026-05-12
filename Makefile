6efa12eac8d5c36ed84494f6eef01e5e6765bd65
Morgan Brethome <morgan.brethome@epitech.eu>
2026-05-12 18:50:50 +0200

    [ADD] suggestion archi

---
+++ Makefile
@@ -0,0 +1,37 @@
+##
+## EPITECH PROJECT, 2026
+## Makefile
+## File description:
+## project makefile
+##
+
+SRC = 
+
+OBJ = $(SRC:.c=.o)
+OBJ := $(OBJ:.cpp=.o)
+
+CFLAGS   = -Wall -Wextra -Werror -pedantic -march=native 
+CXXFLAGS = -Wall -Wextra -Werror -pedantic -march=native -std=c++20 
+LDFLAGS  = -pthread
+NAME = plazza
+
+CC = c++
+
+
+all: $(NAME)
+
+$(NAME): $(OBJ)
+	$(CC) $(OBJ) -o $(NAME) $(LDFLAGS)
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	rm -f $(OBJ)
+
+fclean: clean
+	rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: all clean re
---
+++ include/plazza.h
@@ -0,0 +1,55 @@
+#pragma once
+
+
+
+#include <string>
+namespace Ingredient {
+    enum list_ingr{
+        Dough,
+        Tomato,
+        Eggplant,
+        GoatChesse,
+        ChiefLove,
+    };
+
+    Ingredient::list_ingr to_string(Ingredient::list_ingr ingr);
+    std::string from_string(std::string ingr);
+}
+
+enum PizzaType
+{
+    Regina = 1,
+    Margarita = 2,
+    Americana = 4,
+    Fantasia = 8
+};
+enum PizzaSize
+{
+    S = 1,
+    M = 2,
+    L = 4,
+    XL = 8,
+    XXL = 16
+};
+
+
+class Pizza {
+    public:
+        PizzaType type;
+        PizzaSize size;
+};
+
+
+class Cook {
+    public:
+        Cook();
+        void start();
+};
+
+class Kitchen {
+    public:
+        Kitchen();
+        void add_cook(Cook cook);
+    private:
+        int _multiplier;
+};
